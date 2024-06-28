1481d1a5c11ab7c1fb28b50ad7c0ca928fb267f5
yemmi <105867100+tlroal0305@users.noreply.github.com>
2024-06-28 23:38:41 +0900

    Initial commit

---
+++ src/main/java/com/project/LookAtMe/LookAtMeApplication.java
@@ -0,0 +1,13 @@
+package com.project.LookAtMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookAtMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookAtMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/LookAtMe/LookAtMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.LookAtMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookAtMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
